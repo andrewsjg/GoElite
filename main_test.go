ab28d2fa709c8019d42677d2e97a293f8e14d993
James Andrews <jgandrews@gmail.com>
2022-09-25 17:26:50 +0100

    Fixed first test

--- main_test.go
+++ main_test.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestGalaxy(t *testing.T) {
 	galaxy := initGalaxy(1)
 
-	if galaxy.systems[7].name != "LAVE" {
-		t.Error("System number 7 in galaxy 1 should be LAVE. Got: ", galaxy.systems[7].name)
+	if strings.Compare(strings.ToUpper(galaxy.systems[7].name), "LAVE") == 0 {
+		t.Error("System number 7 in galaxy 1 should be LAVE. Got:", galaxy.systems[7].name)
 	}
 
-	if galaxy.systems[147].name != "DISO" {
-		t.Error("System number 7 in galaxy 1 should be DISO. Got: ", galaxy.systems[147].name)
+	if strings.Compare(strings.ToUpper(galaxy.systems[147].name), "DISO") == 0 {
+		t.Error("System number 147 in galaxy 1 should be DISO. Got:", galaxy.systems[147].name)
 	}
+
 }
