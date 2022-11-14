f020d1b7bef50b4d532469f4e5da6f57b2f0ffd2
James Andrews <jgandrews@gmail.com>
2022-11-14 23:02:04 +0000

    Cleaned up main.go

--- main.go
+++ main.go
@@ -5,17 +5,13 @@ import (
 	"os"
 	"strings"
 
-	engine "github.com/andrewsjg/GoElite/engine"
 	"github.com/andrewsjg/GoElite/internal/tui"
 )
 
 func main() {
 
-	//game := engine.InitGame(false)
-	//debugTests(game)
-
 	// TODO: Probably a better way to encode version required
-	const VER = "0.1.0"
+	const VER = "0.1.4"
 
 	args := os.Args[1:]
 
@@ -29,32 +25,5 @@ func main() {
 	}
 
 	tui.Start()
-
-}
-
-func debugTests(game engine.Game) {
-
-	// Initial State
-	//game.PrintState()
-
-	plan := game.Galaxy.Systems[game.Player.Ship.Location.CurrentPlanet]
-
-	fmt.Println(plan.SprintMarket(game.Commodities))
-
-	/* Jump to DISO
-	err := game.Jump("DISO")
-
-	if err != nil {
-		fmt.Println("Jump failed: " + err.Error())
-	}
-
-	game.PrintState()
-
-	game.PrintLocal()
-
-	fmt.Printf("\nDoing Hyperspace Jump\n\n")
-	game.HyperSpaceJump()
-
-	game.PrintLocal() */
 
 }
